5fd4bc4fd6107e48805677664498757ad3ab8ad7
Lowe Schmidt <github@loweschmidt.se>
2020-04-30 15:37:38 +0200

    Refactor to use data sources.

--- main.tf
+++ main.tf
@@ -1,115 +1,113 @@
-provider "aws" {
-  region  = "${var.aws_region}"
-}
-
 resource "aws_s3_bucket" "security" {
-  bucket_prefix = "${var.bucket_prefix}"
+  bucket_prefix = var.bucket_prefix
   acl           = "private"
-  region        = "${var.aws_region}"
+  region        = var.region
 
   lifecycle {
     prevent_destroy = true
   }
 
-  tags = "${merge(map("Name","Security Bucket"), var.tags)}"
+  tags = merge(map("Name","Security Bucket"), var.tags)
 }
+
+data "aws_iam_policy_document" "enable_guardduty" {
+  statement {
+   actions = [
+   "guardduty:*"
+   ]
+    effect = "Allow"
+    resources = ["*"]
+  }
+  statement {
+    effect = "Allow"
+    actions = ["iam:CreateServiceLinkedRole"]
+    resources = [
+      "arn:aws:iam::${var.aws_account_id}:role/aws-service-role/guardduty.amazonaws.com/AWSServiceRoleForGuardDuty"
+    ]
+    condition {
+      test     = "StringLike"
+      values   = ["guardduty.amazonaws.com"]
+      variable = "iam:AWSServiceName"
+    }
+  }
+  statement {
+    actions = [
+      "iam:PutRolePolicy",
+      "iam:DeleteRolePolicy"
+    ]
+    resources = [
+      "arn:aws:iam::${var.aws_account_id}:role/aws-service-role/guardduty.amazonaws.com/AWSServiceRoleForAmazonGuardDuty"
+    ]
+  }
+}
+
 resource "aws_iam_policy" "enable_guardduty" {
-  name        = "enable-guardduty-${var.aws_region}"
+  name        = "enable-guardduty-${var.region}"
   path        = "/"
   description = "Allows setup and configuration of GuardDuty"
 
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "guardduty:*"
-            ],
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "iam:CreateServiceLinkedRole"
-            ],
-            "Resource": "arn:aws:iam::${var.aws_account_id}:role/aws-service-role/guardduty.amazonaws.com/AWSServiceRoleForAmazonGuardDuty",
-            "Condition": {
-                "StringLike": {
-                    "iam:AWSServiceName": "guardduty.amazonaws.com"
-                }
-            }
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "iam:PutRolePolicy",
-                "iam:DeleteRolePolicy"
-            ],
-            "Resource": "arn:aws:iam::${var.aws_account_id}:role/aws-service-role/guardduty.amazonaws.com/AWSServiceRoleForAmazonGuardDuty"
-        }
-    ]
+  policy = data.aws_iam_policy_document.enable_guardduty.json
 }
-EOF
+
+data "aws_iam_policy_document" "security_bucket" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:ListAllMyBuckets"
+    ]
+    resources = [
+      "arn:aws:s3:::*"
+    ]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:*"
+    ]
+    resources = [
+      aws_s3_bucket.security.arn,
+      "${aws_s3_bucket.security.arn}/*"
+    ]
+  }
 }
 
 resource "aws_iam_policy" "use_security_bucket" {
-  name        = "access-security-bucket-${var.aws_region}"
+  name        = "access-security-bucket-${var.region}"
   path        = "/security/"
   description = "Allows full access to the contents of the security bucket"
 
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": "s3:ListAllMyBuckets",
-      "Resource": "arn:aws:s3:::*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": "s3:*",
-      "Resource": [
-        "${aws_s3_bucket.security.arn}",
-        "${aws_s3_bucket.security.arn}/*"
-      ]
-    }
-  ]
-}
-EOF
+  policy = data.aws_iam_policy_document.security_bucket.json
 }
 
 resource "aws_iam_group" "guardduty" {
-  name = "${var.group_name}-${var.aws_region}"
+  name = "${var.group_name}-${var.region}"
   path = "/"
 }
 
 resource "aws_iam_group_policy_attachment" "enable" {
-  group      = "${aws_iam_group.guardduty.name}"
-  policy_arn = "${aws_iam_policy.enable_guardduty.arn}"
+  group      = aws_iam_group.guardduty.name
+  policy_arn = aws_iam_policy.enable_guardduty.arn
 }
 
 resource "aws_iam_group_policy_attachment" "useS3bucket" {
-  group      = "${aws_iam_group.guardduty.name}"
-  policy_arn = "${aws_iam_policy.use_security_bucket.arn}"
+  group      = aws_iam_group.guardduty.name
+  policy_arn = aws_iam_policy.use_security_bucket.arn
 }
 
 resource "aws_iam_group_policy_attachment" "access" {
-  group      = "${aws_iam_group.guardduty.name}"
+  group      = aws_iam_group.guardduty.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonGuardDutyFullAccess"
 }
 
 resource "aws_iam_group_policy_attachment" "s3readonly" {
-  group      = "${aws_iam_group.guardduty.name}"
+  group      = aws_iam_group.guardduty.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
 }
 
 resource "aws_iam_group_membership" "guardduty" {
-  name  = "guardduty-admin-members-${var.aws_region}"
-  group = "${aws_iam_group.guardduty.name}"
-  users = "${var.users}"
+  name  = "guardduty-admin-members-${var.region}"
+  group = aws_iam_group.guardduty.name
+  users = var.users
 }
 
 resource "aws_guardduty_detector" "guardduty" {
--- variables.tf
+++ variables.tf
@@ -1,27 +1,32 @@
 variable "aws_account_id" {
   description = "The ID of the AWS account to use"
+  type = string
 }
 
-variable "aws_region" {
+variable "region" {
   description = "Which region are we running the module against"
+  type = string
 }
 
 variable "users" {
-  type        = "list"
+  type        = list(string)
   description = "A list, where you can specify your members"
 }
 
 variable "bucket_prefix" {
   default     = "security"
   description = "The prefix to use for the s3 bucket"
+  type = string
 }
 
 variable "group_name" {
   default     = "guardduty-admin"
   description = "The name of guardDuty admins"
+  type = string
 }
 
 variable "tags" {
+  type = map(string)
   default = {
     "owner"   = "DevSecOps"
     "project" = "GuarDuty"
